ecb33bec021c5151b350b266786844a26228d5e1
Matt Schiller <matts@gbs-mac-0733.ev.greenberginc.com>
2015-03-30 16:39:43 -0700

    added basic boxstarter scripts

---
+++ initial.bat
@@ -0,0 +1,16 @@
+@echo off
+
+cinst google-chrome-x64
+cinst 1password
+cinst silverlight
+cinst adobereader
+cinst filezilla
+cinst sumatrapdf
+cinst jre8
+cinst skype
+cinst spotify
+cinst notepadplusplus
+cinst teamviewer
+cinst 7zip
+
+Install-WindowsUpdate -AcceptEula
