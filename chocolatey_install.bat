fd6cb886e9f4e4a7a9948df4c40440aecd718f56
Matt Schiller <matts@gbs-mac-0733.ev.greenberginc.com>
2015-03-31 11:37:53 -0700

    combined unrestricted and installchocolatey

--- chocolatey_install.bat
+++ chocolatey_install.bat
@@ -1,3 +1,4 @@
 @echo off
 
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "Set-ExecutionPolicy Unrestricted -Force"
